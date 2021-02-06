0c6a5a56d911a3614f68f282bd7b7ee49242510e
SUBINI <SUBINI@DESKTOP-LILG0S0>
2021-02-07 04:17:23 +0900

    1. SWAGGER 설정 클래스 정의 2. API 요청 결과에 따른 응답 데이터 구조 정형화

--- src/main/java/org/eboard/global/config/SwaggerConfig.java
+++ src/main/java/org/eboard/global/config/SwaggerConfig.java
@@ -1,11 +1,17 @@
 package org.eboard.global.config;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,18 +21,34 @@ public class SwaggerConfig {
 	
 	@Bean
 	public Docket api() {
-		return null;
+		return new Docket(DocumentationType.SWAGGER_2)
+				.consumes(getConsumeContentTypes())
+				.produces(getProduceContentTypes())
+				.apiInfo(getApiInfo())
+				.select()
+				.paths(PathSelectors.ant("/e-board/api/**"))
+				.build();
 	}
 	
+	
 	private Set<String> getConsumeContentTypes() {
-		return null;
+		Set<String> consumes = new HashSet<>();
+		consumes.add("application/json;charset=UTF-8");
+		consumes.add("application/x-www-form-urlencoded");
+		return consumes;
 	}
 	
 	private Set<String> getProduceContentTypes() {
-		return null;
+		Set<String> produces = new HashSet<>();
+		produces.add("application/json;charset=UTF-8");
+		return produces;
 	}
 	
 	private ApiInfo getApiInfo() {
-		return null;
+		return new ApiInfoBuilder()
+				.title("E-BOARD API")
+				.description("[ZeonLec] API")
+				.contact(new Contact("ZeonLec Swagger", "https://github.com/zeonlec/e-board", "tnqls3718@gmail.com"))
+				.version("1.0")				.build();
 	}
 }
